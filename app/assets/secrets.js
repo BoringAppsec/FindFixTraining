a6f6f740dfaadd7275bf2cf83fee7c0d9b02973c
Saurabh Kumar <admin@Saurabhs-MacBook-Air.local>
2022-11-12 10:04:21 +0530

    updated secrets.js

--- app/assets/secrets.js
+++ app/assets/secrets.js
@@ -1,6 +1,5 @@
-secret=abcd 
-token="1234567890"
-api_key="0987654321"
+User, Access key ID, Secret access key
+awsowaspuser, AKIAVG6YRIQWCC2CQFOK, qxrmjoH3KUltb4smN4SvxZtRwOpPQCIctmwXLNmh
 
 // Admin Account - u:admin p:Admin_123
 // User Accounts (u:user1 p:User1_123), (u:user2 p:User2_123)

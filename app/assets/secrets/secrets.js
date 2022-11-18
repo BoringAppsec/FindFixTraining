f05e70e48a07217e2c68c0642b5ee9295f9bc110
Saurabh Kumar <admin@Saurabhs-MacBook-Air.local>
2022-11-18 21:21:36 +0530

    changes

--- app/assets/secrets/secrets.js
+++ app/assets/secrets/secrets.js
@@ -1,5 +1,5 @@
 User, Access key ID, Secret access key
-awsowaspuser, AKIAVG6YRIQWCC2CQFOK, qxrmjoH3KUltb4smN4SvxZtRwOpPQCIctmwXLNmh
+awsowaspuser, AKIAVG6YRIQWMFJ7FIG6, 1LmKirz245IsLJAKiw/qNlyFfTJOsagmgSXiPwQT
 
 // Admin Account - u:admin p:Admin_123
 // User Accounts (u:user1 p:User1_123), (u:user2 p:User2_123)
